e78ae96252e33011df0eaca50ef3b3eafe231d0b
Nasser Naif <nmaif7788@hotmail.com>
2025-03-06 02:08:06 +0300

    finish all LinkedList methods

--- DS/LinkedList/src/main/java/org/example/LinkedList.java
+++ DS/LinkedList/src/main/java/org/example/LinkedList.java
@@ -22,9 +22,11 @@ public class LinkedList {
             System.out.println(current.getData());
             current = current.getNext();
         }
+        System.out.println("The size of this LinkedList is : " + size);
     }
 
-    public void append(Node newNode) {
+    public void append(String value) {
+        Node newNode = new Node(value);
         if (size == 0) {
             tail = newNode;
             head = newNode;
@@ -56,7 +58,7 @@ public class LinkedList {
             }
             return temp;
 
-            
+
 //            another solution
 //            Node newLastItem = head;
 //
@@ -71,6 +73,142 @@ public class LinkedList {
 //
 //            size--;
 //            return newLastItem;
+        }
+    }
+
+    public void prepend(String value) {
+        Node newNode = new Node(value);
+
+        if (size == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.setNext(head);
+            head = newNode;
+        }
+
+        size++;
+    }
+
+    public Node removeFirst() {
+        if (size == 0) return null;
+
+        Node removedNode = head;
+        head = head.getNext();
+        removedNode.setNext(null);
+        size--;
+
+        if (size == 0) {
+            tail = null;
+        }
+        return removedNode;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+
+        Node temp = head;
+
+        for (int i = 0; i < index; i++) {
+            temp = temp.getNext();
+        }
+
+        return temp;
+
+        // old solution
+
+//        int count = 0;
+//        Node current = this.head;
+//
+//        while (current != null) {
+//            if (index == count) {
+//                return current;
+//            } else {
+//                current = current.getNext();
+//                count++;
+//            }
+//        }
+//        return null;
+    }
+
+    public boolean set(int index, String value) {
+
+        Node temp = get(index);
+
+        if (temp == null) {
+            return false;
+        }
+        temp.setData(value);
+        return true;
+
+
+        // Other Solution
+//        if (index < 0 || index >= size) {
+//            return false;
+//        }
+//        Node temp = head;
+//        for (int i = 0; i < index; i++) {
+//            temp = temp.getNext();
+//        }
+//        temp.setData(value);
+//        return true;
+    }
+
+    public boolean insert(int index, String value) {
+        if (index < 0 || index > size) return false;
+
+        if (index == 0) {
+            prepend(value);
+            return true;
+        } else if (index == size) {
+            append(value);
+            return true;
+        } else {
+            Node newNode = new Node(value);
+            Node temp = get(index - 1);
+
+            newNode.setNext(temp.getNext());
+            temp.setNext(newNode);
+            size++;
+            return true;
+        }
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= size) return null;
+
+        if (index == 0) {
+            return removeFirst();
+        } else if (index == size - 1) {
+            return removeLats();
+        } else {
+            Node removedItem = get(index);
+            Node temp = get(index - 1);
+
+            temp.setNext(removedItem.getNext());
+            removedItem.setNext(null);
+            size--;
+            return removedItem;
+        }
+
+    }
+
+
+    public void reverse() {
+        Node temp = head;
+        head = tail;
+        tail = temp;
+
+        Node after = temp.getNext();
+        Node before = null;
+
+        for (int i = 0; i < size; i++) {
+            after = temp.getNext();
+            temp.setNext(before);
+            before = temp;
+            temp = after;
         }
     }
 
--- DS/LinkedList/src/main/java/org/example/Main.java
+++ DS/LinkedList/src/main/java/org/example/Main.java
@@ -7,7 +7,8 @@ public class Main {
         LinkedList linkedList = new LinkedList("Nasser");
 
 
-        linkedList.append(new Node("Khalid"));
+        linkedList.append("Khalid");
+        linkedList.append("Naif");
 //        linkedList.print();
 //
 //        System.out.println("Head : " + linkedList.getHead());
@@ -15,19 +16,64 @@ public class Main {
 //        System.out.println("Size : " + linkedList.getSize());
 
 
-        // remove last item in linked list with 2 items
-        System.out.println("############ remove last item in linked list of 2 item ############");
-        System.out.println("removed item is : " + linkedList.removeLats().getData());
+//        // remove last item in linked list with 2 items
+//        System.out.println("############ remove last item in linked list of 2 item ############");
+//        System.out.println("removed item is : " + linkedList.removeLats().getData());
+//
+//
+//        // remove last item in linked list with 1 items
+//        System.out.println("############ remove last item in linked list of 1 item ############");
+//        System.out.println("removed item is : " + linkedList.removeLats().getData());
+//
+//
+//        // remove last item in linked list with 0 items
+//        System.out.println("############ remove last item in linked list of 0 item ############");
+//        System.out.println("removed item is : " + linkedList.removeLats());
 
 
-        // remove last item in linked list with 1 items
-        System.out.println("############ remove last item in linked list of 1 item ############");
-        System.out.println("removed item is : " + linkedList.removeLats().getData());
+        // prePend method
+//
+//        linkedList.prepend("Ahmed");
+//        linkedList.print();
+
+        // removed first Item
+//        System.out.println("Removed first item: " + linkedList.removeFirst().getData());
+//        linkedList.print();
+
+        // get item by index
+//        System.out.println("get item by index 1: " + linkedList.get(1).getData());
+
+        // set value in item with specific index
+//        System.out.println("Linked List Before set new value in index 1");
+//        linkedList.print();
+//        System.out.println("The value changed ? :" + linkedList.set(1, "Mohammed"));
+//        System.out.println("Linked List After set new value in index 1");
+//        linkedList.print();
 
 
-        // remove last item in linked list with 0 items
-        System.out.println("############ remove last item in linked list of 0 item ############");
-        System.out.println("removed item is : " + linkedList.removeLats());
+        // insert a new Node in the LinkedList
+//        System.out.println("Linked List Before insert new Node in index 1");
+//        linkedList.print();
+//        System.out.println("The value changed ? :" + linkedList.insert(1, "Mohammed"));
+//        System.out.println("Linked List After insert new Node in index 1");
+//        linkedList.print();
+
+
+        // Remove by index
+//        System.out.println("Linked List Before remove Node in index 1");
+//        linkedList.print();
+//        System.out.println("The value that removed ? : " + linkedList.remove(1).getData());
+//        System.out.println("Linked List After remove Node in index 1");
+//        linkedList.print();
+
+
+        // Reverse LinkedList
+        System.out.println("Linked List Before reverse");
+        linkedList.print();
+        linkedList.reverse();
+        System.out.println("Linked List After reverse");
+        linkedList.print();
+
 
     }
 }
